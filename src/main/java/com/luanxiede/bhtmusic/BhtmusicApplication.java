15ebf137d85289f1393fba18d901534930a95c77
wangdeyun <2798119246@qq.com>
2022-02-03 13:30:40 +0800

    初始化项目

---
+++ src/main/java/com/luanxiede/bhtmusic/BhtmusicApplication.java
@@ -0,0 +1,13 @@
+package com.luanxiede.bhtmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhtmusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BhtmusicApplication.class, args);
+    }
+
+}
